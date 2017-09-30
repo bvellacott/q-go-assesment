678d4d8bf23bc9dd711ac777d0871049962577ab
Benjamin Vellacott <bmvellacott@gmail.com>
2017-09-30 20:34:45 +0100

    test for passing state and dispatch to items list from store

--- src/components/ItemsList/tests/itemsList.test.js
+++ src/components/ItemsList/tests/itemsList.test.js
@@ -3,12 +3,21 @@ import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import FilterButton from '../FilterButton';
 import { ItemsList } from '../index';
+import configureStore from 'redux-mock-store'
+
+const mockStore = configureStore([])
 
 const defaultProps = {
   items: [],
+  filterType: 'all'
 };
 
 describe('ItemsList', () => {
+  let setFilter
+  jest.mock('../../../logic/actions', () => {
+    setFilter = (...args) => setFilter(args)
+  }, { virtual: true })
+
   it('renders without crashing', () => {
     shallow(<ItemsList {...defaultProps} />);
   });
@@ -43,6 +52,32 @@ describe('ItemsList', () => {
     expect(filterButtons.at(0).props().onClick()).toEqual('all')
     expect(filterButtons.at(1).props().onClick()).toEqual('complete')
     expect(filterButtons.at(2).props().onClick()).toEqual('incomplete')
+
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+
+  it('passes items and merged filter actions from store', () => {
+    const store = mockStore({
+      todos: {
+        items: [1, 2, 3],
+        filter: 'all',
+      }
+    })
+    setFilter = type => type
+
+    const wrapper = shallow(<ItemsList store={store} />);
+
+    const passedProps = wrapper.props()
+    expect(passedProps.items).toEqual([1, 2, 3])
+    expect(passedProps.filter).toBeUndefined()
+
+    expect(typeof passedProps.filterAll).toEqual('function')
+    expect(typeof passedProps.filterComplete).toEqual('function')
+    expect(typeof passedProps.filterIncomplete).toEqual('function')
+
+    expect(typeof passedProps.filterAll()).toEqual('all')
+    expect(typeof passedProps.filterComplete()).toEqual('complete')
+    expect(typeof passedProps.filterIncomplete()).toEqual('incomplete')
 
     expect(toJson(wrapper)).toMatchSnapshot()
   })
