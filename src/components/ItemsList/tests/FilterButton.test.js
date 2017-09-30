b7f5424f234d9402aa976167544ba9d2107e87fb
Benjamin Vellacott <bmvellacott@gmail.com>
2017-09-30 19:29:49 +0100

    dead filter button tests

---
+++ src/components/ItemsList/tests/FilterButton.test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import FilterButton from '..'
+
+describe('filter button', () => {
+	it('Sets button text', () => {
+		throw new Error('test not implemented')
+	})
+
+	it('calls setFilter on click', () => {
+		throw new Error('test not implemented')
+	})
+})
