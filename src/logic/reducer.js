59e0c3b714ef6c2a61a24e6fa150ae9358fe23b9
Benjamin Vellacott <bmvellacott@gmail.com>
2017-10-01 00:12:09 +0100

    implement reducer

--- src/logic/reducer.js
+++ src/logic/reducer.js
@@ -1,13 +1,18 @@
-import { ADD_ITEM } from './constants';
+import { 
+  ADD_ITEM,
+  REMOVE_ITEM,
+  TOGGLE_ITEM_COMPLETE,
+  SET_FILTER,
+} from './constants'
 
-let nextId = 3;
+let nextId = 3
 
 export const initialState = {
   items: [
     { id: 1, content: 'Make sure items are completeable' },
     { id: 2, content: 'Add filters (Use HOC)' },
   ],
-};
+}
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
@@ -15,15 +20,31 @@ const reducer = (state = initialState, action) => {
       const newItem = {
         id: nextId++,
         content: action.content,
-      };
+      }
 
       return {
         ...state,
         items: [...state.items, newItem],
-      };
+      }
+    case REMOVE_ITEM:
+      return {
+        ...state,
+        items: state.items.filter(item => item.id !== action.id)
+      }
+    case TOGGLE_ITEM_COMPLETE:
+      return {
+        ...state,
+        items: state.items.map(item => item.id === action.id ? 
+          { ...item, complete: !item.complete } : item),
+      }
+    case SET_FILTER:
+      return {
+        ...state,
+        filter: action.filter,
+      }
     default:
-      return state;
+      return state
   }
-};
+}
 
-export default reducer;
+export default reducer
